7b17b40724a705858a81978a88e0a42deb9adfc1
ninpeng <ninpeng@naver.com>
2019-09-04 21:38:02 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import resolvers from './resolvers';
 
 const server = new GraphQLServer({
   typeDefs: 'src/schema.graphql',
-  resolvers
+  resolvers,
+  options: { static: 'build' }
 })
 
 server.start({port: 80}, () => console.log('server start'));
