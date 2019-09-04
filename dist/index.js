3f6b37e07a52450a14e1bada332a6ed5b54f2528
ninpeng <ninpeng@naver.com>
2019-09-04 18:46:49 +0900

    GraphQL init
    초기 세팅 및 babel 7 설정함

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _graphqlYoga = require("graphql-yoga");
+console.log('oo')
+
+// var server = new _graphqlYoga.GraphQLServer({});
+// server.start(function () {
+//   return console.log('server start');
+// });
