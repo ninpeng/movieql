7c25015d871ea37d61411d987f7b8232e724b70b
ninpeng <ninpeng@naver.com>
2019-09-11 14:03:22 +0900

    movies 쿼리 변경
    스키마에 MovieList  타입 추가

--- src/api.js
+++ src/api.js
@@ -5,9 +5,9 @@ const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
 const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getMovies = async (limit=10, page=1, minimum_rating=5) => {
+export const getMovies = async (limit=10, page=1, minimum_rating='') => {
   const response = await axios.get(LIST_MOVIES_URL, { params: { limit, page, minimum_rating } });
-  return response.data.data.movies;
+  return response.data.data;
 }
 
 export const getMovie = async (movie_id) => {
